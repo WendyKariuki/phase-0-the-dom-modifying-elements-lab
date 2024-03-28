d43a029f38df52204af97eefec8b9151506c7256
WendyKariuki <phaminuswendy@gmail.com>
2024-03-28 23:37:46 +0300

    DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement("h1")
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = `WENDY is the champion`;
